c6973a74b5ed4363aa6d5733e48ce27476458d3e
rkkingswood <rachel.kingswood@smartdatasystems.net>
2017-06-06 17:05:16 -0400

    Checkpoint08 Complete

--- Bloc/ruby-practice/controllers/menu_controller.rb
+++ Bloc/ruby-practice/controllers/menu_controller.rb
@@ -75,7 +75,7 @@ class MenuController
       end
     end
 
-   def create_entry
+    def create_entry
      system "clear"
      puts "New AddressBloc Entry"
 
@@ -89,14 +89,66 @@ class MenuController
      address_book.add_entry(name, phone, email)
      system "clear"
      puts "New entry created"
-   end
+    end
 
-   def search_entries
+    def search_entries
+      print "Search by name: "
+      name = gets.chomp
+
+      match = address_book.binary_search(name)
+      system "clear"
+
+      if match
+        puts match.to_s
+        search_submenu(match)
+      else
+        puts "No match found for #{name}"
+      end
+    end
+
+    def delete_entry(entry)
+     address_book.entries.delete(entry)
+     puts "#{entry.name} has been deleted"
+    end
+
+    def edit_entry(entry)
+     print "Updated name: "
+     name = gets.chomp
+     print "Updated phone number: "
+     phone_number = gets.chomp
+     print "Updated email: "
+     email = gets.chomp
+
+     entry.name = name if !name.empty?
+     entry.phone_number = phone_number if !phone_number.empty?
+     entry.email = email if !email.empty?
+     system "clear"
+
+     puts "Updated entry:"
+     puts entry
+    end
    end
 
    def read_csv
+     print "Enter CSV file to import: "
+     file_name = gets.chomp
 
+     if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+       entry_count = address_book.import_from_csv(file_name).count
+       system "clear"
+       puts "#{entry_count} new entries added from #{file_name}"
+     rescue
+       puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+       read_csv
+     end
    end
+
    def entry_submenu(entry)
      puts "n - next entry"
      puts "d - delete entry"
@@ -110,8 +162,10 @@ class MenuController
      when "n"
 
      when "d"
+      delete_entry(entry)
      when "e"
-
+      edit_entry(entry)
+      entry_submenu(entry)
      when "m"
        system "clear"
        main_menu
@@ -121,4 +175,29 @@ class MenuController
        entry_submenu(entry)
      end
    end
+
+   def search_submenu(entry)
+     puts "\nd - delete entry"
+     puts "e - edit this entry"
+     puts "m - return to main menu"
+     selection = gets.chomp
+
+     case selection
+       when "d"
+         system "clear"
+         delete_entry(entry)
+         main_menu
+       when "e"
+         edit_entry(entry)
+         system "clear"
+         main_menu
+       when "m"
+         system "clear"
+         main_menu
+       else
+         system "clear"
+         puts "#{selection} is not a valid input"
+         puts entry.to_s
+         search_submenu(entry)
+    end
  end
